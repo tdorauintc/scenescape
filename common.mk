de9da030136d5984b9a56277c109a389fc2193f1
Tomasz Dorau <tomasz.dorau@intel.com>
2025-06-03 17:37:03 +0200

    Apply suggestions from code review - license headers
    Co-authored-by: Lukasz Talarczyk <lukasz.talarczyk@intel.com>

--- common.mk
+++ common.mk
@@ -1,13 +1,7 @@
-# Copyright (C) 2025 Intel Corporation
-#
-# This software and the related documents are Intel copyrighted materials,
-# and your use of them is governed by the express license under which they
-# were provided to you ("License"). Unless the License provides otherwise,
-# you may not use, modify, copy, publish, distribute, disclose or transmit
-# this software or the related documents without Intel's prior written permission.
-#
-# This software and the related documents are provided as is, with no express
-# or implied warranties, other than those that are expressly stated in the License.
+# SPDX-FileCopyrightText: (C) 2025 Intel Corporation
+# SPDX-License-Identifier: LicenseRef-Intel-Edge-Software
+# This file is licensed under the Limited Edge Software Distribution License Agreement.
+# See the LICENSE file in the root of this repository for details.
 
 SHELL := /bin/bash
 VERSION:=$(shell cat ../version.txt)
