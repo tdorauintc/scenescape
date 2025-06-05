d10763aa8e14ee6ba5989987f92d44c8d5ccc1b3
Tomasz Dorau <tomasz.dorau@intel.com>
2025-06-05 07:49:30 +0200

    Remove pruning script

--- auto-prune.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for folder in autocalibration controller manager percebro; do
-  python3 prune_requirements.py "make -C $folder" $folder/requirements-buildtime.txt $folder/requirements-runtime.txt
-done
